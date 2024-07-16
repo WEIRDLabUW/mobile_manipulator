eb5c43dad82c2b15226382e6f52ee163b0315674
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2024-07-15 17:32:25 -0700

    Combine right_js_info into one message

--- src/mm_client/mm_client/mm_client_node.py
+++ src/mm_client/mm_client/mm_client_node.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Twist
-from geometry_msgs.msg import Pose
-from mm_interfaces.msg import JoystickButtons
+from mm_interfaces.msg import OculusJoystickInfo
 from .OculusController import OculusController
 
 class MMClientNode(Node):
@@ -11,31 +9,21 @@ class MMClientNode(Node):
 		self.get_logger().info('Initialized MM Client')
 		self.oculus_controller = OculusController()
 		print('MM Client: Oculus Controller Initialized-----')
-		self.right_js_pos_pub = self.create_publisher(Pose, '/right_js_position', 10)
-		self.right_js_button_pub = self.create_publisher(JoystickButtons, '/right_js_buttons', 10)
-		self.timer = self.create_timer(0.5, self.publishJsData)
+		self.right_js_info_pub = self.create_publisher(OculusJoystickInfo, '/right_js_info', 10)
+		self.timer = self.create_timer(0.1, self.publishJsInfo)
 
-	def publishJsData(self):
-		self.publishJSPosition()
-		self.publishJSButtons()
-
-	def publishJSPosition(self):
+	def publishJsInfo(self):
 		pos = self.oculus_controller.getCurrentPosition()
-		pose = Pose()
-		pose.position.x = pos[0]
-		pose.position.y = pos[1]
-		pose.position.z = pos[2]
-		self.right_js_pos_pub.publish(pose)
-
-	def publishJSButtons(self):
 		buttons = self.oculus_controller.getButtons()
-		joystick_buttons = JoystickButtons()
-		joystick_buttons.a = buttons['A']
-		joystick_buttons.b = buttons['B']
-		joystick_buttons.rtr = buttons['RTr']
-		joystick_buttons.rg = buttons['RG']
-		self.right_js_button_pub.publish(joystick_buttons)
-
+		info = OculusJoystickInfo()
+		info.pose.position.x = pos[0]
+		info.pose.position.y = pos[1]
+		info.pose.position.z = pos[2]
+		info.a = buttons['A']
+		info.b = buttons['B']
+		info.rtr = buttons['RTr']
+		info.rg = buttons['RG']
+		self.right_js_info_pub.publish(info)
 
 def main(args=None):
 	rclpy.init()
