2800c2e8cdb8831bfd9a9e2ca0bc6f15b25c83b5
Numfor Mbiziwo-Tiapo <numsmt2@gmail.com>
2024-07-15 20:03:27 -0700

    Send left joystick messages

--- src/mm_client/mm_client/mm_client_node.py
+++ src/mm_client/mm_client/mm_client_node.py
@@ -1,30 +1,50 @@
 import rclpy
 from rclpy.node import Node
+from .oculus_reader.oculus_reader.reader import OculusReader
 from mm_interfaces.msg import OculusJoystickInfo
-from .OculusController import OculusController
 
 class MMClientNode(Node):
 	def __init__(self):
 		super().__init__('mm_client_node')
 		self.get_logger().info('Initialized MM Client')
-		self.oculus_controller = OculusController()
+		self.oculus = OculusReader()
 		self.get_logger().info('MM Client: Oculus Controller Initialized-----')
 		self.right_js_info_pub = self.create_publisher(OculusJoystickInfo, '/right_js_info', 10)
+		self.left_js_info_pub = self.create_publisher(OculusJoystickInfo, '/left_js_info', 10)
 		self.timer_duration = 0.02 # 50 hz
 		self.timer = self.create_timer(self.timer_duration, self.publishJsInfo)
 
 	def publishJsInfo(self):
-		pos = self.oculus_controller.getCurrentPosition()
-		buttons = self.oculus_controller.getButtons()
+		transformations, buttons = self.oculus.get_transformations_and_buttons()
+		right_js_info = self.getRightJSInfo(transformations, buttons)
+		left_js_info = self.getLeftJSInfo(transformations, buttons)
+		self.right_js_info_pub.publish(right_js_info)
+		self.left_js_info_pub.publish(left_js_info)
+
+	def getRightJSInfo(self, transformations, buttons):
+		positions = transformations['r'][:,3]
 		info = OculusJoystickInfo()
-		info.pose.position.x = pos[0]
-		info.pose.position.y = pos[1]
-		info.pose.position.z = pos[2]
+		info.pose.position.x = positions[0]
+		info.pose.position.y = positions[1]
+		info.pose.position.z = positions[2]
 		info.a = buttons['A']
 		info.b = buttons['B']
 		info.rtr = buttons['RTr']
 		info.rg = buttons['RG']
-		self.right_js_info_pub.publish(info)
+		return info
+
+	def getLeftJSInfo(self, transformations, buttons):
+		positions = transformations['l'][:,3]
+		info = OculusJoystickInfo()
+		info.pose.position.x = positions[0]
+		info.pose.position.y = positions[1]
+		info.pose.position.z = positions[2]
+		info.x = buttons['X']
+		info.y = buttons['Y']
+		info.ltr = buttons['LTr']
+		info.lg = buttons['LG']
+		return info
+
 
 def main(args=None):
 	rclpy.init()
